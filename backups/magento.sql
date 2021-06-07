0474ea93bcf65a19b83037c84fbd0b80c08f5d7c
pqthinh <phamquangquang2008@gmail.com>
2021-06-07 17:11:37 +0700

    update db

--- backups/magento.sql
+++ backups/magento.sql
@@ -1,4 +1,3 @@
-mysqldump: Error: 'Access denied; you need (at least one of) the PROCESS privilege(s) for this operation' when trying to dump tablespaces
 -- MySQL dump 10.18  Distrib 10.3.27-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: db    Database: magento
